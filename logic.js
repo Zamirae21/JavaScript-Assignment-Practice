7ad079cf67b7b04c4a025f1dc0d618d340c55e23
Zamira Estrada <156048180+Zamirae21@users.noreply.github.com>
2024-03-18 13:37:01 -0400

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,11 @@
+// Declare variables
+var firstName = "Zamira";
+var lastName = "Estrada";
+var age = 20;
+
+// Fire alerts with messages using variables
+alert("My name is " + firstName + " " + lastName);
+alert("I am " + age + " years old");
+
+// Log a message in the console
+console.log("I'm having so much fun in this class!");
